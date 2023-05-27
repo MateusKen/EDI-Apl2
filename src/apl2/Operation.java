fa2961ec7c57b5a5e2856fd39009cc2563d5ab4c
Erik Hsu <111096802+erikhsu08@users.noreply.github.com>
2023-05-27 16:23:47 -0300

    Update Operation.java

--- src/apl2/Operation.java
+++ src/apl2/Operation.java
@@ -3,7 +3,11 @@
 //*************************** ATENÇÃO! *****************************
 // arquivo: src/apl2/Operation.java
 
-// TODO: Colocar a identificação dos(as) integrantes aqui.
+// NOME: Enrico Cuono Alves Pereira		TIA: 32258798
+// NOME: Erik Samuel Viana Hsu			TIA: 32265921
+// NOME: Mateus Kenzo Iochimoto			TIA: 32216289
+// NOME: Rodrigo Machado de Assis Oliveira de Lima		TIA: 32234678
+// NOME: Thiago Shihan Cardoso Toma		TIA: 32210744
 
 package apl2;
 
